6f5b54872068e90a34a5c286455ab6ec41107744
jehat armanç deniz <45170719+jehato47@users.noreply.github.com>
2020-04-12 13:16:27 +0300

    Update models.py

--- article/models.py
+++ article/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from embed_video.fields import EmbedVideoField
 
 # Create your models here.
 
@@ -7,9 +8,10 @@ from ckeditor.fields import RichTextField
 class Article(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE, verbose_name="Yazar")
     title = models.CharField(max_length=50, verbose_name="Başlık")
-    content = RichTextField()
+    content = RichTextField(verbose_name="İçerik")
     created_date = models.DateTimeField(auto_now_add=True, verbose_name="Oluşturulma Tarihi")
-    article_image = models.FileField(blank=True, null=True, verbose_name="Makaleye Fotoğraf Ekleyin")
+    # article_image = models.FileField(blank=True, null=True, verbose_name="Makaleye Fotoğraf Ekleyin")
+    article_image = EmbedVideoField()
 
     def __str__(self):
         return self.title
@@ -27,5 +29,5 @@ class Comment(models.Model):
     def __str__(self):
         return self.comment_content
 
-    class Meta:  # Buna da bak
+    class Meta:  # Buna da  bak
         ordering = ["-comment_date"]
